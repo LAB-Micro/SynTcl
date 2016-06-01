299cb6ef1f1c88b697c7adda41731527180caeb1
server <server@email.com>
2016-06-01 15:54:03 +0200

    test

---
+++ test.tcl
@@ -0,0 +1,16 @@
+ proc leakage_opt {} {
+	set wcep [get_object_name [get_attribute [get_timing_paths] endpoint]]
+	puts "Worst 5 critical paths ending in ${wcep}"
+	foreach_in_collection path [get_timing_paths -to $wcep -nworst 5 -slack_greater_than 0] {
+    		set tmp_ep [get_object_name [get_attribute $path endpoint]]
+    		set tmp_ar [get_attribute $path arrival]
+    		set tmp_sl [get_attribute $path slack]
+    		
+		puts "endpoint:	$tmp_ep"
+    		puts "arrival:	$tmp_ar"
+    		puts "slack:	$tmp_sle"
+    		puts ""
+		report_attribute $path
+
+  }
+}
