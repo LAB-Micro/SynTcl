85bbf6868542c9c2bffa887fa4d94762389c89a2
server <server@email.com>
2016-06-02 21:29:49 +0200

    cells of a path in test.tcl

--- test.tcl
+++ test.tcl
@@ -1,17 +1,12 @@
  proc leakage_opt {} {
-	set wcep [get_object_name [get_attribute [get_timing_paths] endpoint]]
-	puts "Worst 5 critical paths ending in ${wcep}"
-	foreach_in_collection path [get_timing_paths -to $wcep -nworst 5 -slack_greater_than 0] {
-    		set tmp_ep [get_object_name [get_attribute $path endpoint]]
-    		set tmp_ar [get_attribute $path arrival]
-    		set tmp_sl [get_attribute $path slack]
-    		
-		puts "endpoint:	$tmp_ep"
-    		puts "arrival:	$tmp_ar"
-    		puts "slack:	$tmp_sl"
-    		puts ""
-		report_attribute $path
-		#get_attributes
+	set timing_path [get_timing_path  -to [all_outputs]]
+	set timing_points [get_attr $timing_path points]
 
-  }
+	foreach_in_collection this_point $timing_points {
+	echo "object: [get_attr $this_point object]"
+	echo "object_name: [get_object_name [get_attr $this_point object]]"
+	echo "get_cell: [get_cells -of_obj [get_object_name [get_attr $this_point object]]]"
+	echo "cell_name: [get_object_name [get_cells -of_obj [get_object_name [get_attr $this_point object]]]]"
+	echo "------------"
+}
 }
